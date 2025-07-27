54f1e3f32eeb6f043ad39fa2a08944d9a4f10e51
66025009 <150766111+66025009@users.noreply.github.com>
2025-07-27 15:23:09 +0700

    Display total users and equipment on Dashboard

--- src/app/(page)/menu/dashboard/page.tsx
+++ src/app/(page)/menu/dashboard/page.tsx
@@ -1,1 +1,34 @@
-// แดชบอร์ด -ผู้ดูแลระบบครุภัณฑ์
+import { StatsCard } from "@/components/StatsCard";
+import { users, equipmentCategories } from "@/lib/data";
+
+const Dashboard = () => {
+    const totalInternalUsers = users.filter((user) => user.role === "internal").length;
+    const totalExternalUsers = users.filter((user) => user.role === "external").length;
+    const totalKaruphan = equipmentCategories.length;
+
+    return (
+        <main className="dashboard wrapper py-8">
+            <section className="flex flex-col gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
+                    <StatsCard
+                        total={totalKaruphan}
+                        headerTitle="จำนวนครุภัณฑ์"
+                        bgColor="bg-BlueLight"
+                    />
+                    <StatsCard
+                        total={totalInternalUsers}
+                        headerTitle="จำนวนพนักงานในแผนก"
+                        bgColor="bg-Green"
+                    />
+                    <StatsCard
+                        total={totalExternalUsers}
+                        headerTitle="จำนวนพนักงานนอกแผนก"
+                        bgColor="bg-Green"
+                    />
+                </div>
+            </section>
+        </main>
+    );
+};
+
+export default Dashboard;
